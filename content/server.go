43030c801da5743fa59d292b6bde8b7cd852b182
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-08-10 08:18:39 +0200

    upgraded server + upgraded diagram

--- content/server.go
+++ content/server.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./docs")))
+	fmt.Println("Server running on 127.0.0.1:8080")
 	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Server running on 127.0.0.1:8080")
 }
